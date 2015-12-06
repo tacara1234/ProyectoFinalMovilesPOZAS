47d3d45a98eb5b2d66b3673650cabc8620e750fc
Lalo_Canche <eduardo_benji@hotmail.com>
2015-12-05 21:21:12 -0600

    Cambios menores.

--- www/js/database.js
+++ www/js/database.js
@@ -12,10 +12,14 @@ function initDataBase() {
 
 function populateDB(tx) {
     tx.executeSql('DROP TABLE IF EXISTS ponds');
-    tx.executeSql('CREATE TABLE IF NOT EXISTS ponds (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, latitude, altitude, name, path_image)');
-    tx.executeSql('INSERT INTO ponds (id, latitude, altitude, name, path_image) VALUES (1,"55.3","43.7", "Poza 1", "path_image")');
-    tx.executeSql('INSERT INTO ponds (id, latitude, altitude, name, path_image) VALUES (2,"55.3","43.7","Poza 2","path_image")');
-    tx.executeSql('INSERT INTO ponds (id, latitude, altitude, name, path_image) VALUES (3,"55.3","43.7", "Poza 3","path_image")');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS ponds (id INTEGER' +
+        ' PRIMARY KEY AUTOINCREMENT NOT NULL, latitude, altitude, name, path_image)');
+    tx.executeSql('INSERT INTO ponds (id, latitude, altitude, name, path_image) ' +
+        'VALUES (1,"55.3","43.7", "Poza 1", "path_image")');
+    tx.executeSql('INSERT INTO ponds (id, latitude, altitude, name, path_image) ' +
+        'VALUES (2,"55.3","43.7","Poza 2","path_image")');
+    tx.executeSql('INSERT INTO ponds (id, latitude, altitude, name, path_image) ' +
+        'VALUES (3,"55.3","43.7", "Poza 3","path_image")');
 }
 function errorDB(err) {
     alert("Error processing SQL: " + err.code + ", " + err.message);
@@ -35,18 +39,12 @@ function querySuccess(tx, results) {
     var status = document.getElementById("divListPonds");
     var string = "Pozas: " + len + "<br/>";
     string += "<table><tr><td>ID</td>" +
-            //"<td>Latitude</td>" +
-            //"<td>Altitude</td>" +
         "<td>Nombre de la poza</td>" +
-            //"<td>Path Image</td>" +
         "</tr>";
     for (var i = 0; i < len; i++) {
         string +=
             "<tr><td>" + results.rows.item(i).id + "</td>" +
-                //"<td>" + results.rows.item(i).latitude + "</td>" +
-                //"<td>" + results.rows.item(i).altitude + "</td>" +
             "<td>" + results.rows.item(i).name + "</td>" +
-                //"<td>" + results.rows.item(i).path_image + "</td>" +
             "</tr>";
     }
     status.innerHTML = string;
@@ -85,7 +83,6 @@ function delete_pond(pond_id) {
 }
 
 function delete_from_db(tx) {
-    //DELETE FROM ponds WHERE id=pond_id;
     tx.executeSql('DELETE FROM ponds WHERE id=?', [id_pond_to_delete]);
 
 }
