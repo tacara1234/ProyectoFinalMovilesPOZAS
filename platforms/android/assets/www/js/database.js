736fd27d726520c6d6beb10fd3c8c286bbbdbcc8
Lalo_Canche <eduardo_benji@hotmail.com>
2015-12-08 18:13:38 -0600

    corrida de android

--- platforms/android/assets/www/js/database.js
+++ platforms/android/assets/www/js/database.js
@@ -13,7 +13,7 @@ function initDataBase() {
 function createDB(tx) {
     //tx.executeSql('DROP TABLE IF EXISTS ponds');
     tx.executeSql('CREATE TABLE IF NOT EXISTS ponds (id INTEGER' +
-        ' PRIMARY KEY AUTOINCREMENT NOT NULL, latitude, altitude, name, path_image)');
+            ' PRIMARY KEY AUTOINCREMENT NOT NULL, latitude, altitude, name, path_image)');
     //Lo siguiente son datos dummy, si los queremos solo descomentamos lo sig:
     //tx.executeSql('INSERT INTO ponds (id, latitude, altitude, name, path_image) ' +
     //    'VALUES (1,"55.3","43.7", "Poza 1", "Test Path")');
@@ -36,32 +36,118 @@ function queryDB(tx) {
 function querySuccess(tx, results) {
     //first get the number of rows in the result set
     var len = results.rows.length;
+    var listado = "";
+    var popUps = "";
     var status = document.getElementById("divListPonds");
     localStorage.setItem("last_id", len + 1);
     if (len == 0) {
         status.innerHTML = "No hay pozas registradas.";
     } else {
-        var string = "Pozas: " + len + "<br/>";
-        string += "<table><tr><td>Número</td>" +
-            "<td>Nombre de la poza</td>" +
-            "</tr>";
+        //var string = "Pozas: " + len + "<br/>";
+        //string += "<table><tr><td>Número</td>" +
+        //    "<td>Nombre de la poza</td>" +
+        //    "</tr>";
         for (var i = 0; i < len; i++) {
-            string +=
-                "<tr><td>" + results.rows.item(i).id + "</td>" +
-                "<td>" + results.rows.item(i).name + "</td>" +
-                "</tr>";
+            listado += "<li data-icon='gear'><a href='#opciones" + i +
+                    "' data-rel='popup' data-position-to='window' " +
+                    "data-transition='pop'>" + results.rows.item(i).id + ") " + results.rows.item(i).name +
+                    "</a></li>";
+
+            // string +=
+            //   "<tr><td>" + results.rows.item(i).id + "</td>" +
+            //   "<td>" + results.rows.item(i).name + "</td>" +
+            //   "</tr>";
+            var options = "opciones" + i;
+            var deleteOperation = "delop" + i;
+            var editOperation = "editop" + i;
+            var seeOperation = "seeop" + i;
+
+            popUps += "<div data-role='popup' id='" + options + "' data-theme='a' data-overlay-theme='b' " +
+                    "class='ui-content' style='max-width:340px; padding-bottom:2em;'> " +
+                    "<h3>Escoja la operacion</h3>" +
+                    "<input id='" + deleteOperation + "' type='button' data-icon='delete' " +
+                    "value='eliminar' />" +
+                    "<input id='" + editOperation + "' type='button' data-icon='edit' " +
+                    "value='modificar' />" +
+                    "<input id='" + seeOperation + "' type='button' data-icon='eye' " +
+                    "value='modificar' /></div>";
+
         }
-        status.innerHTML = string;
+        //status.innerHTML = string;
+
+        actualizarLista(listado);
+        crearPopUps(popUps);
+
+        for (var counter1 = 0; counter1 < len; counter1++) {
+            (function (counter1) {
+                //It is added the delete operation as 'onclick' in each link
+                document.getElementById('delop' + counter1).
+                        addEventListener("click", function () {
+                            delete_pond(results.rows.item(counter1).id);
+                        });
+            }(counter1));
+
+        }
+
+        
+        for (var counter2 = 0; counter2 < len; counter2++) {
+            (function (counter2) {
+                //It is added the the function that will set the info from the read file, in the view
+                document.getElementById('editop' + counter2).
+                        addEventListener("click", function () {
+                            //Aqui se tiene que setear la info y de la poza en base al id y que esta se
+                            //pueda modificar
+                            //setInfoFromFile(entries[k].name);
+                            pruebaFuncion(results.rows.item(counter2).id);
+                        });
+            }(counter2));
+
+        }
+
+        for (var counter3 = 0; counter3 < len; counter3++) {
+            (function (counter3) {
+                //It is added the the function that will set the info from the read file, in the view
+                document.getElementById('seeop' + counter3).
+                        addEventListener("click", function () {
+                            get_pond(results.rows.item(counter3).id);
+                            //setInfoFromFile(entries[k].name);
+                        });
+            }(counter3));
+
+        }
     }
 
 }
 
+function pruebaFuncion(msgID){
+    alert("El id de esta poza es: " + msgID);
+}
+
+var listcreated = false;
+function actualizarLista(datos) {
+    if (!listcreated) {
+        var divList = $("#divListPonds");
+        divList.append("<ul id='lista' data-role='listview' data-inset='true' data-filter='true' " +
+                "data-filter-placeholder='buscar..'></ul>");
+        listCreated = true;
+        divList.trigger("create");
+    }
+    var list = $("#lista");
+    list.append(datos);
+    list.listview("refresh");
+}
+
+function crearPopUps(popups) {
+    var divPopups = $('#popups');
+    divPopups.append(popups);
+    divPopups.trigger("create");
+}
 
 function guargarDatosPoza() {
 
     if ($('#nombre_poza_alta').val() == '' ||
-        $('#latitud_poza_alta').val() == '' ||
-        $('#longitud_poza_alta').val() == '') {
+            $('#latitud_poza_alta').val() == '' ||
+            $('#longitud_poza_alta').val() == '') {
         alert("Llene todos los campos por favor. ");
         return;
     }
@@ -83,10 +169,10 @@ function save_new_pond(tx) {
     var photo_image = window.localStorage.getItem("photo");
 
     tx.executeSql(
-        'INSERT INTO ponds (id, latitude, altitude, name, path_image)' +
-        ' VALUES ( ? , ? , ? , ?, ? )',
-        [id, latitude, longitude, name, photo_image]
-    );
+            'INSERT INTO ponds (id, latitude, altitude, name, path_image)' +
+            ' VALUES ( ? , ? , ? , ?, ? )',
+            [id, latitude, longitude, name, photo_image]
+            );
 
 
     window.location.href = "#listaPozas";
@@ -123,7 +209,7 @@ function get_pond(id) {
 
 function get_pond_from_db(tx) {
     tx.executeSql('SELECT * FROM ponds WHERE id=?',
-        [id_pond], querySelectSuccess, errorDB);
+            [id_pond], querySelectSuccess, errorDB);
 
 }
 
